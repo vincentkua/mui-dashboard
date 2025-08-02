716497eb70161158ea8747247a4b68088f9a4f54
vincentkua <zylyn89@gmail.com>
2025-08-02 16:23:35 +0800

    init : init and configured based on latest package dependency

---
+++ src/Providers.tsx
@@ -0,0 +1,11 @@
+import Dashboard from "./pages/dashboard/Dashboard";
+
+const Providers = () => {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+};
+
+export default Providers;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import Button from "@mui/material/Button";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+    </div>
+  );
+};
+
+export default Home;
